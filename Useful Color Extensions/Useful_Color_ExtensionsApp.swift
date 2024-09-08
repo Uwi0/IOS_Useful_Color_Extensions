ea7e81789d3ca128aec6549a0cf62d22f927e90e
uwi0 <kakapoweb@gmail.com>
2024-09-08 07:53:23 +0700

    Initial Commit

---
+++ Useful Color Extensions/Useful_Color_ExtensionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Useful_Color_ExtensionsApp.swift
+//  Useful Color Extensions
+//
+//  Created by dwi prasetyo on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Useful_Color_ExtensionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
